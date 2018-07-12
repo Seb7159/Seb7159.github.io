6b619129b0549a0f01bf3d8ee33020fd9b7fc585
CronScheduler <lipsa.identificare@yahoo.com>
2018-07-12 13:46:55 +0100

    Manual IP add

--- upload.sh
+++ upload.sh
@@ -1,6 +1,19 @@
 cd /home/pi/GitHub/Seba7159.github.io 
-curl icanhazip.com > ip.txt
-git add *
-git commit -m "Message" --allow-empty
-git push
+
+# echo "Waiting 1 minute..."
+# sleep 1m 
+
+if [ "$(cat ip.txt)" != "$(curl icanhazip.com)" ] 
+then
+	echo "IP has been changed. "
+	echo "Old IP was $(cat ip.txt). "
+	echo " New IP is $(curl icanhazip.com)" 
+
+	#Send new IP on github project  
+	sleep 1m
+	curl icanhazip.com > ip.txt
+	git add ip.txt 
+	git commit -m "Cron job updated Raspberry's IP" --allow-empty
+	git push
+fi
 
